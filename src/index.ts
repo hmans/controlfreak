ded9e5f3bec71f7ba057d3be961956bc6a7ba1cc
Hendrik Mans <hendrik@mans.de>
2022-03-11 19:23:54 +0100

    Add steps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./Controller"
 export * from "./Control"
 export * from "./devices"
+export * from "./steps"
 
 export type Vector = { x: number; y: number }
---
+++ src/steps.ts
@@ -0,0 +1,33 @@
+import { VectorControl } from "."
+import { KeyboardDevice, GamepadDevice } from "./devices"
+
+export const normalizeVector = ({
+  value
+}: VectorControl) => {
+  const length = Math.sqrt(value.x ** 2 + value.y ** 2) || 1
+  value.x /= length
+  value.y /= length
+}
+
+export const compositeKeyboardVector =
+  (up: string, down: string, left: string, right: string) =>
+  ({ value, controller }: VectorControl) => {
+    if (controller.activeDevice instanceof KeyboardDevice) {
+      const { isPressed } = controller.activeDevice
+      value.x = isPressed(right) - isPressed(left)
+      value.y = isPressed(up) - isPressed(down)
+    }
+  }
+
+export const gamepadAxisVector =
+  (horizontalAxis = 0, verticalAxis = 1) =>
+  ({ value, controller }: VectorControl) => {
+    if (controller.activeDevice instanceof GamepadDevice) {
+      const { device } = controller.activeDevice
+
+      if (device) {
+        value.x = device.axes[horizontalAxis]
+        value.y = -device.axes[verticalAxis]
+      }
+    }
+  }
