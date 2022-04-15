9d307c1277ec666a8637f10d4d248f9d4e59c00f
Hendrik Mans <hendrik@mans.de>
2022-04-15 19:13:07 +0200

    Comments and cleanup

--- packages/controlfreak/src/Controller.ts
+++ packages/controlfreak/src/Controller.ts
@@ -1,37 +1,31 @@
 import { Control } from "."
-import {
-  Device,
-  GamepadDevice,
-  KeyboardDevice
-} from "./devices"
+import { Device, GamepadDevice, KeyboardDevice } from "./devices"
 
 export class Controller {
+  /** A list of devices driving this controller. */
   devices: Device[] = [
     new KeyboardDevice().start(),
     new GamepadDevice().start()
   ]
+
+  /** The currently active device. */
   activeDevice: Device = null!
 
+  /** The controls defined by this controller.  */
   controls: Record<string, Control> = {}
 
   constructor() {
     for (const device of this.devices) {
-      device.onActivity.on(
-        () => (this.activeDevice = device)
-      )
+      device.onActivity.on(() => (this.activeDevice = device))
     }
   }
 
   update() {
     for (const device of this.devices) device.update()
-    for (const control of Object.values(this.controls))
-      control.update()
+    for (const control of Object.values(this.controls)) control.update()
   }
 
-  addControl(
-    name: string,
-    klass: { new (...args: any[]): Control }
-  ) {
+  addControl(name: string, klass: { new (...args: any[]): Control }) {
     const instance = new klass(this)
     this.controls[name] = instance
     return instance
