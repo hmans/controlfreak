0584dedfaa38cac9950709c7b4c481f8b2dc9bdc
Hendrik Mans <hendrik@mans.de>
2022-04-15 17:33:36 +0200

    Simplify

--- packages/controlfreak/src/lib/vectorish.ts
+++ packages/controlfreak/src/lib/vectorish.ts
@@ -7,12 +7,10 @@ export const magnitude = ({ x, y }: IVector2) => Math.sqrt(x * x + y * y)
 
 export const normalize = (v: IVector2) => divide(v, magnitude(v) || 1)
 
-export const scale = (v: IVector2, f: number) => {
+export const multiply = (v: IVector2, f: number) => {
   v.x *= f
   v.y *= f
   return v
 }
 
-export const multiply = scale
-
-export const divide = (v: IVector2, f: number) => scale(v, 1 / f)
+export const divide = (v: IVector2, f: number) => multiply(v, 1 / f)
