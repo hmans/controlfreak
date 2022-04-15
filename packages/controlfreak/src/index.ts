35343ebfe69d9b6a59e2f5ff8fbb3f434685d4ba
Hendrik Mans <hendrik@mans.de>
2022-04-15 20:00:56 +0200

    Eh

--- packages/controlfreak/src/index.ts
+++ packages/controlfreak/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./Controller"
 export * from "./Control"
 export * from "./devices"
-export * as processors from "./Processors"
+export * as processors from "./processors"
 
 export type { IVector2 } from "./lib/vectorish"
