9c7a9ee745c2114314752ab35944451e6fe7fc69
Hendrik Mans <hendrik@mans.de>
2022-04-16 12:43:15 +0200

    Update signal library

--- packages/controlfreak/src/devices/index.ts
+++ packages/controlfreak/src/devices/index.ts
@@ -1,3 +1,4 @@
 export * from "./Device"
-export * as keyboard from "./keyboard"
 export * as gamepad from "./gamepad"
+export * as keyboard from "./keyboard"
+export * as touch from "./touch"
---
+++ packages/controlfreak/src/devices/touch.ts
@@ -0,0 +1,39 @@
+import { Signal } from "@hmans/signal"
+import { Device } from "./Device"
+
+export class TouchDevice extends Device {
+  onTouchStart = Signal<TouchEvent>()
+  onTouchEnd = Signal<TouchEvent>()
+  onTouchMove = Signal<TouchEvent>()
+
+  start() {
+    document.addEventListener("touchstart", this.handleTouchStart)
+    document.addEventListener("touchend", this.handleTouchEnd)
+    document.addEventListener("touchmove", this.handleTouchMove)
+    return this
+  }
+
+  stop() {
+    document.removeEventListener("touchstart", this.handleTouchStart)
+    document.removeEventListener("touchend", this.handleTouchEnd)
+    document.removeEventListener("touchmove", this.handleTouchMove)
+    return this
+  }
+
+  handleTouchStart = (e: TouchEvent) => {
+    this.onActivity.emit()
+    this.onTouchStart.emit(e)
+  }
+
+  handleTouchEnd = (e: TouchEvent) => {
+    this.onActivity.emit()
+    this.onTouchEnd.emit(e)
+  }
+
+  handleTouchMove = (e: TouchEvent) => {
+    this.onActivity.emit()
+    this.onTouchMove.emit(e)
+  }
+
+  update() {}
+}
